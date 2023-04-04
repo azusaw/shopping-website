e2b1b5589c7f85efd3e1933ea2663b1065eaa08f
azusaw <azusa0304@gmail.com>
2023-04-04 15:06:51 +0100

    connect item and image data and display

--- shopping/models.py
+++ shopping/models.py
@@ -1,17 +1,7 @@
 from django.db import models
 
 
-class Item(models.Model):
-    id = models.TextField(primary_key=True, unique=True)
-    gender = models.ForeignKey('Gender', on_delete=models.CASCADE)
-    master_category = models.ForeignKey('MasterCategory', on_delete=models.CASCADE)
-    sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
-    article_type = models.ForeignKey('ArticleType', on_delete=models.CASCADE)
-    base_colour = models.ForeignKey('BaseColour', on_delete=models.CASCADE)
-    season = models.TextField()
-    year = models.IntegerField()
-    usage = models.TextField()
-    display_name = models.TextField()
+
 
 
 class Gender(models.Model):
@@ -50,8 +40,21 @@ class BaseColour(models.Model):
         return self.id
 
 
+class Item(models.Model):
+    id = models.TextField(primary_key=True, unique=True)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    master_category = models.ForeignKey('MasterCategory', on_delete=models.CASCADE)
+    sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
+    article_type = models.ForeignKey('ArticleType', on_delete=models.CASCADE)
+    base_colour = models.ForeignKey('BaseColour', on_delete=models.CASCADE)
+    season = models.TextField()
+    year = models.IntegerField()
+    usage = models.TextField()
+    display_name = models.TextField()
+
+
 class Image(models.Model):
-    item = models.ForeignKey('Item', on_delete=models.CASCADE, primary_key=True, unique=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, primary_key=True, unique=True)
     link = models.TextField(null=True, blank=True)
 
     def __str__(self):
--- shopping/views.py
+++ shopping/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from .models import Item
+from .models import Item, Image
 
 
 def item_list(request):
-    items = Item.objects.all()
-    return render(request, 'all.html', {'items': items, 'cnt': len(items)})
+    items_with_image = Image.objects.select_related("item").all()
+    return render(request, 'all.html', {'items_with_image': items_with_image, 'cnt': len(items_with_image)})
 
