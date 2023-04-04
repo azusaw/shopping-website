88da21ff8369383232ff8a222ec420d54cae8bdb
azusaw <azusa0304@gmail.com>
2023-04-04 18:22:43 +0100

    create header and side-bar

--- shopping/urls.py
+++ shopping/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list, name=''),
+    path('', views.item_list, name='home'),
     path('all/', views.item_list, name='all'),
     path('detail/<slug:item_id>', views.item_detail, name='detail'),
 ]
