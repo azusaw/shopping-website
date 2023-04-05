87b263d57d0044783867aa0dc706c2ae005a77a3
azusaw <azusa0304@gmail.com>
2023-04-05 14:55:25 +0100

    create keyword search, modify search conditions

--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,4 @@
+import webcolors
 from django.db.models import Q
 from django.shortcuts import render
 
@@ -22,24 +23,37 @@ def menu():
 
 
 def item_list(request):
+    title = "All item"
+    keyword = ""
     gender = ""
     master = ""
     sub = ""
     colour = ""
 
     # Set value if query string exists
+    if "keyword" in request.GET:
+        keyword = request.GET["keyword"]
+        title = f"Keyword: {keyword}"
     if "gender" in request.GET:
         gender = request.GET["gender"]
+        title = f"Gender: {gender}"
     if "master" in request.GET:
         master = request.GET["master"]
+        title = f"Category: {master}"
     if "sub" in request.GET:
         sub = request.GET["sub"]
+        title = f"Sub Category: {sub}"
     if "colour" in request.GET:
         colour = request.GET["colour"]
+        # Covert hex colour code to colour name for display
+        colour_name = webcolors.hex_to_name(f"#{request.GET['colour']}")
+        title = f"Colour: {colour_name.capitalize()}"
 
     # Create WHERE clause from query strings
     where = []
     where_color = []
+    if keyword != '':
+        where.append(Q(item__display_name__contains=keyword))
     if gender != '':
         where.append(Q(item__gender=gender))
     if master != '':
@@ -51,8 +65,8 @@ def item_list(request):
 
     items_with_image = Image.objects.all().select_related("item").filter(*where)
     # FIX ME
-    items = items_with_image.select_related("item__base_colour").filter(*where_color)
-    return render(request, 'all.html', {'menu': menu(), 'items': items, 'cnt': len(items)})
+    items = items_with_image.select_related("item__base_colour").filter(*where_color)[:10]
+    return render(request, 'item_list.html', {'title': title, 'menu': menu(), 'items': items, 'cnt': len(items)})
 
 
 def item_detail(request, item_id):
